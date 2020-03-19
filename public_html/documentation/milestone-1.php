f59da836b7833cee397a083ce1fb962f561cf511
Nathan Ortiz <nkortiz92@gmail.com>
2020-03-19 15:50:20 -0600

    Fixed some spacing and spelling errors

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,43 +11,43 @@
 	<div>
 		<h2>Purpose, Audience & Goal</h2>
 		<p>
-			<strong>Purpose:</strong>This project will be a website that allows people to look up New Mexican dishes and
-			their recipes. It is based off the Facebook group called "New Mexico Recipes
+			<strong>Purpose:</strong> This project will be a website that allows people to look up New Mexican dishes and
+			their recipes. It is based off the Facebook group called "New Mexico Recipes".
 		</p>
 		<p>
-			<strong>Audience:</strong>This website is targeted to individuals who like and are interested in New Mexican dishes
+			<strong>Audience:</strong> This website is targeted to individuals who like and are interested in New Mexican dishes
 			and how to make them. Target audience includes: members of the facebook group "New Mexico Recipes", ages 16-65 years old,
 			cooks, and people who reside or have resided in New Mexico.
 		</p>
 		<p>
-			<strong>Goal:</strong>The primary goal of this site is for the users of the site to be able to access the recipes
+			<strong>Goal:</strong> The primary goal of this site is for the users of the site to be able to access the recipes
 			I have for New Mexican cuisine and that are posted on the Facebook group "New Mexico Recipes".
 		</p>
 	</div>
 	<div>
 		<h2>Persona</h2>
-		<p><strong>Name:</strong>Natalie Ortiz</p>
-		<p><strong>Age:</strong>35</p>
-		<p><strong>Profession:</strong>Works at Enterprise Bank & Trust as a teller in Santa Fe, New Mexico.</p>
+		<p><strong>Name:</strong> Natalie Ortiz</p>
+		<p><strong>Age:</strong> 35</p>
+		<p><strong>Profession:</strong> Works at Enterprise Bank & Trust as a teller in Santa Fe, New Mexico.</p>
 		<p>
-			<strong>Technology:</strong>Windows user. Uses a HP Envy with Intel Core i5, a Surface Pro, and a Samsung S10
+			<strong>Technology:</strong> Windows user. Uses a HP Envy with Intel Core i5, a Surface Pro, and a Samsung S10
 			with unlimited 4G data connection.
 		</p>
 		<p>
-			<strong>Attitudes and Behaviors:</strong>Natalie is a mother of 2 boys ages 10 and 13 years old. Her job is Monday
+			<strong>Attitudes and Behaviors:</strong> Natalie is a mother of 2 boys ages 10 and 13 years old. Her job is Monday
 			through Friday from 8am to 5pm. Natalie is always on the go and has a full schedule. She drops off her sons at work,
-			goes to work, picks up her sons from an after school program after work. She enjoys cooking for her sons with her
+			goes to work, and picks up her sons from an after school program after work. She enjoys cooking for her sons with her
 			husband. Is an avid social media user and is apart of the Facebook group New Mexico recipes.
 		</p>
 		<p>
-			<strong>Goals and Needs:</strong>Natalie enjoys the cuisine in New Mexico and likes how nightly dinner brings her family
-			together. Her goal is to find new recipes on New Mexican Cuisine to cook for her family. Natalie likes the Facebook
+			<strong>Goals and Needs:</strong> Natalie enjoys the cuisine in New Mexico and likes how nightly dinner brings her family
+			together. Her goal is to find new recipes on New Mexican cuisine to cook for her family. Natalie likes the Facebook
 			group "New Mexico Recipes" but needs a faster and more organized way to find new recipes rather than scrolling through
 			the groups timeline.
 		</p>
 		<p>
-			<strong>User Story:</strong>As a user, I need to quickly find new recipes on New Mexico cuisine on my laptop or
-			cell phone.
+			<strong>User Story:</strong> As a user, I need to quickly find new recipes on New Mexico cuisine on my laptop or
+			cell phone to cook for me and my family.
 		</p>
 	</div>
 </body>
