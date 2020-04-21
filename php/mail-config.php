2847a5f161d9225e5afbd105267e9e24723b792d
Nathan Ortiz <nkortiz92@gmail.com>
2020-04-21 13:56:45 -0600

    code along with cecilia. hooking up mailgun

---
+++ php/mail-config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * mail-config.php
+ * This file contains your reCAPTCHA API keys and your recipient's email addresses.
+ *
+ * @param string $siteKey your public reCAPTCHA API key
+ * @param string $secret your secret reCAPTCHA API key
+ * @param string $mailgunDomain your mailgun private api key.
+ * @param string $mailgunApiKey your mailgun domain relay.
+ * @param array $MAIL_RECIPIENTS array of email addresses and corresponding recipient names to send form responses to
+ *
+ * @author Rochelle Lewis <rlewis37@cnm.edu>
+ *
+ * This file contains sensitive information and should ALWAYS be gitignored!
+ **/
+
+//mailgun API Key authorization
+
+$mailgunApiKey = "13951635fd8273ac7b004a797fd475d6-f135b0f1-24d3370e";
+$mailgunDomain = "https://app.mailgun.com/app/sending/domains/sandbox30282ec56f4947f39769aa9961227b75.mailgun.org";
+
+
+// your Google reCAPTCHA keys here
+// your Google reCAPTCHA keys here
+$siteKey = '6Ld6BOwUAAAAAHC5l4HOfmEcjiEHyx8zG7a7_LeP';
+$secret = '6Ld6BOwUAAAAAJ3fFug4-oivTDKeSOK_YZenqfw4';
+
+/**
+ * attach the recipients to the message
+ * notice this an array that can include or omit the the recipient's real name
+ * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+ **/
+$MAIL_RECIPIENTS = [ "sickknate25@gmail.com" => "Nathan Ortiz"];
+$MAIL_RECIPIENT = ["sickknate25@gmail.com"];
